12a5e919b657498a02e9c30c9161af477350d2ee
Rupam Pakhira <79349072+liorupam123@users.noreply.github.com>
2022-01-24 12:29:32 +0530

    Create 02.Recursive_Binary_Search.cpp

---
+++ 02.Recursive_Binary_Search.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/binary-search/
+
+class Solution {
+public:
+    int recursiveBS(vector<int>& nums,int left,int right, int target){
+      
+      while(left<=right){
+        int mid =left+(right-left)/2;
+      
+        if(nums[mid]==target){ 
+            return mid;
+        }
+        else if(nums[mid]>target){ 
+            return recursiveBS(nums,left,mid-1,target);
+        }
+        else{ 
+            return recursiveBS(nums,mid+1,right,target);
+        }
+      }
+      return -1;  
+    }
+    int search(vector<int>& nums, int target) {
+      int n = nums.size();
+      return recursiveBS(nums,0,n-1,target);
+      
+    }
+  
+};
